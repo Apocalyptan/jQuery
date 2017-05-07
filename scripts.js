e6be1052a02b0193cd5be335faa0e75b02bdbe5a
Adam Rudny <rudnyadam@gmail.com>
2017-05-07 18:40:09 +0200

    Add Zadanie 10.3

--- scripts.js
+++ scripts.js
@@ -1,22 +1,50 @@
 $(document).ready(function() {
 	var carouselList = $('#carousel ul');
-
-	//Carousel Auto-Change
-	setInterval(changeSlides, 3000);
-
-	/*
-	 * Carousel Changing Functions
-	 */
-	function changeSlides() {
-		carouselList.animate({'marginLeft': -750}, 1000, moveFirstSlide);
-	};
 	
+	setInterval(nextSlide, 7000); /Slide Auto Change
+
+	/**
+	 * Carousel Functions
+	 */
 	function moveFirstSlide() {
 		var firstItem = carouselList.find('li:first');
 		var lastItem = carouselList.find('li:last');
 
 		lastItem.after(firstItem);
 
-		carouselList.css({marginLeft:0});
+		carouselList.css({marginLeft: 0});
 	};
+
+	function moveLastSlide() {
+		var firstItem = carouselList.find('li:first');
+		var lastItem = carouselList.find('li:last');
+
+		firstItem.before(lastItem);
+
+		carouselList.css({marginLeft: -400});
+	};
+
+	/**
+	* Carousel Slide Change Animations
+	*/
+	function prevSlide() {
+		moveLastSlide();
+		carouselList.animate({'marginLeft': 0}, 500);
+	};
+
+	function nextSlide() {
+		carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);
+	};
+
+	/**
+ 	 *	Arrow Click Functions
+ 	 */
+	$(".fa-arrow-left").click(function() {
+		prevSlide();
+	});
+
+	$(".fa-arrow-right").click(function() {
+		nextSlide();
+	});
+
 });
